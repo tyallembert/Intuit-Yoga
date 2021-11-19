58929acd81c2bcee861118dbca538f206ab23bb7
Ty Allembert <tyallembert@gmail.com>
2021-11-19 17:43:25 -0500

    worked on css of header

--- header.php
+++ header.php
@@ -1,3 +1,18 @@
+<?php
+    $file = PATH_PARTS['filename'];
+?>
 <header>
-    <h1>Intuit Yoga and Educational Services</h1>
+    <?php if($file == 'index'){
+            print '<h1>Intuit Yoga<br>and<br>Educational Services</h1>';
+        }
+        elseif($file == 'yogaClasses'){
+            print '<h1>Yoga Services</h1>';
+            print '<figure><img src="images/yoga.png"></figure>';
+            print '<div> Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik"> monkik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>';
+        } 
+        elseif($file == 'teacherCourses'){
+            print '<h1>Teacher Services</h1>';
+            print '<figure><img src="images/teacher.png"></figure>';
+            print '<div> Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik"> monkik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>';
+        }?>
 </header>
--- yogaClasses.php
+++ yogaClasses.php
@@ -1,15 +1,12 @@
 <?php
 include 'top.php';
 
+include 'header.php'
 ?>
-<header>
-    <h2>Yoga Services</h2>
-</header>
 <main>
     <section class = "calenderContainer">
 
     </section>
-    <p>Example</p>
 </main>
 <?php
 include 'footer.php';
