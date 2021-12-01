4d97faa9bb53ef39ec5d26b7fc5725462e4a4499
Ty Allembert <tyallembert@gmail.com>
2021-12-01 13:27:16 -0500

    restyling the main index page

--- index.php
+++ index.php
@@ -3,18 +3,35 @@ include 'top.php';
 
 include 'header.php';
 ?>
-<main>
-    <figure class = 'mollyImage'>
-        <img src = 'images/molly.jpg'>
-    </figure>
+<main class = "indexMain">
+    <section class = "mollyImageSection">
+        <figure class = 'mollyImage'>
+            <img src = 'images/molly-nb.png'>
+        </figure>
+        <section class = "mollyInfo">
+            <h2>Molly Stoner</h2>
+            <p>500hr Yoga Tchr. Training - HTS Yoga</p>
+            <p>M. Ed. Antioch New England</p>
+            <p>B.A. Carleton College</p>
+        </section>
+    </section>
+    <section class = "quickLinks">
+        <a href = "yogaClasses.php"><p>Yoga Classes Signup</p></a>
+        <a href = "teacherCourses.php"><p>Teacher Trainings Signup</p></a>
+    </section>
     <section class = 'firstParagraph'>
         <p>An elementary teacher for over 30 years, Molly Stoner brings to her work a vast experience with guiding and observing students, as well as an intuitive, warm presence.  She has been practicing yoga for over two decades and has been certified to teach yoga since 2013.</p>
-    </section>
-    <figure class = 'yurtImage'>
-        <img src = 'images/yurt.jpg'>
-    </figure>
-    <section class = 'secondParagraph'>
         <p>Molly’s vision is to offer to others the calm centering that yoga has given her. “I want to provide opportunities for renewal, equanimity and spontaneous joy that yoga has given to me through the years,” she shared. She is particularly interested in supporting fellow teachers seeking to refill their wells.</p>
+        <p>Molly offers yoga classes, teacher trainings, and educational support services such as advocacy, mediation, and coaching.</p>
+    </section>
+    <section class = "yurtImageSection">
+        <figure class = 'yurtImage'>
+            <img src = 'images/yurt.jpg'>
+        </figure>
+    </section>
+    <section class = 'secondParagraph'>
+        <p>Intuit Yoga and Educational Services is located on a 19 acre wooded hillside in southern Vermont. With extensive gardens, a babbling brook, and gentle vistas, it offers a feeling of renewal immediately upon arrival.</p>
+        <p>Classes are held in a traditional 25’ yurt, nestled between the gardens and woods. After classes, students are welcome to enjoy a cup of tea, take a walk in the woods, or sit by the stream and still the mind.</p>
     </section>
 </main>
 <?php
